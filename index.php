68a704ddefd3e6466cd5a2e65527f9370697abed
han0ki <43035482+han0ki@users.noreply.github.com>
2019-05-31 21:23:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Itelon</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.icon" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+</head>
+<body>
+<div class="postblock">
+	<div class="container">
+		<div class="postpadding">
+			<p>
+				Ищите конкретный партномер?
+			</p>
+			<div class="postinput">
+				<input type="text" value="" placeholder="Введите партномер или описание товара">
+			</div>
+			<div class="buts">
+			        <button type="submit" class="but">
+			        	Искать
+			        </button>
+			        <i class="fas fa-angle-right" style=""></i>
+		    </div>
+	    </div>
+    </div>
+</div>
+<div class="postmain">
+    <div class="postcomfort">
+        <p>Удобные инструменты - правильный выбор!</p>
+	</div>
+	<div class="container">
+		<div class="postservices">
+			<div class="icons">
+            <img src="img/ico01.png" >
+		</div>
+		<div class="posttext">
+			<span>Конфигуратор серверов</span>
+		</div>
+	</div>
+	<div class="postservices">
+			<div class="icons">
+            <img src="img/ico02.png" >
+		</div>
+		<div class="posttext">
+			<span>Конфигуратор сетевого оборудования</span>
+		</div>
+	</div>
+	<div class="posttext2">
+		<span>Хотите проще?</span>Мы поможем!
+	</div>
+		<div class="postzapros">
+			<div class="postins">
+				<span>Или просто отправьте нам запрос</span>
+							        <button type="submit" class="but1">
+			        	Отправить нам запрос
+			        </button>
+			        <span class="texts">Менеджер свяжется за 15 минут</span>
+			</div>
+	</div>
+	<div class="postservices">
+			<div class="icons">
+            <img src="img/ico03.png" >
+		</div>
+		<div class="posttext">
+			<span>Быстро подобрать сервер</span>
+		</div>
+	</div>
+	<div class="postservices">
+			<div class="icons">
+            <img src="img/ico04.png" >
+		</div>
+		<div class="posttext">
+			<span>Выбрать СХД</span>
+		</div>
+	</div>
+	<div class="postservices">
+			<div class="icons">
+            <img src="img/ico05.png" >
+		</div>
+		<div class="posttext">
+			<span>Взять оборудование в демо</span>
+		</div>
+	</div>
+</div>
+</div>
+
+
+
+
+<script src="js/materialize.js"></script>
+<script src="js/materialize.min.js"></script>
+<script src="js/materiallize097.js"></script>
+<script src="js/materiallize100.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
